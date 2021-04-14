3be544e56a2de6b64219dad279dfef7481a69f73
Groumpfyy <no@email>
2021-04-14 14:02:45 -0500

    Preserve/merge silo contents/state when recreating the single silo

--- scenarios/Frontier Extended/frontier-extended.lua
+++ scenarios/Frontier Extended/frontier-extended.lua
@@ -3,7 +3,7 @@
 --  I did some dumb stuff to make it work for 0.16
 --  added an infinite ocean to the left so the map cannot be cheezed like the old one scenario
 
--- 2020-05-02 Groumpyy
+-- 2020-05-02 Groumpfyy
 --  - Added /movesilo command and /refreshsilo commands
 --  - Made the silo moves work on chunks that have already been generated
 
@@ -33,13 +33,72 @@ local seed_global_xy = function()
   end 
 end
 
+
+-- Delete all silos, recreate 1 new one at the coordinate (preserve inventory)
 local refresh_silo = function()
   seed_global_xy()
 
+  -- Remove all silos blindly, we count the output inventory so we don't lose science
   local surface = game.get_surface(1)
+
+  local input_inventory = {}
+  local output_inventory = {}
+  local rocket_inventory = {}
+  local module_inventory = {}
+  local rocket_parts = 0
+
   for _, entity in pairs(surface.find_entities_filtered{name = "rocket-silo"}) do
+    local i = nil
+
+    -- What module the entity has
+    i = entity.get_module_inventory()
+    if i ~= nil then
+      for n,c in pairs(i.get_contents()) do
+        if module_inventory[n] == nil then module_inventory[n] = 0 end
+        module_inventory[n] = module_inventory[n] + c
+      end
+    end
+
+    -- What is in the input
+    i = entity.get_inventory(defines.inventory.assembling_machine_input)
+    if i ~= nil then
+      for n,c in pairs(i.get_contents()) do
+        if input_inventory[n] == nil then input_inventory[n] = 0 end
+        input_inventory[n] = input_inventory[n] + c
+      end
+    end
+
+    -- What is in the output slot
+    i = entity.get_output_inventory()
+    if i ~= nil then
+      for n,c in pairs(i.get_contents()) do
+        if output_inventory[n] == nil then output_inventory[n] = 0 end
+        output_inventory[n] = output_inventory[n] + c
+      end
+    end
+
+    -- What is in the rocket (if there is one)
+    i = entity.get_inventory(defines.inventory.rocket_silo_rocket)
+    if i ~= nil then
+      for n,c in pairs(i.get_contents()) do
+        if rocket_inventory[n] == nil then rocket_inventory[n] = 0 end
+        rocket_inventory[n] = rocket_inventory[n] + c
+        if c > 0 then rocket_parts = 100 end -- There is something in a rocket, so it's ready to go
+      end
+    end
+
+    if entity.status == defines.entity_status.preparing_rocket_for_launch or
+       entity.status == defines.entity_status.waiting_to_launch_rocket  or
+       entity.status == defines.entity_status.launching_rocket then rocket_parts = 100 end -- so that we put the new silo in Preparing for launch
+
+    if entity.rocket_parts then rocket_parts = rocket_parts + entity.rocket_parts end
+
     entity.destroy()
   end
+
+  if rocket_parts > 100 then rocket_parts = 100 end -- Make sure we don't insert more than the max
+
+  -- Clean the destination area so we can actually create the silo there, hope you didn't have anything there
   for _, entity in pairs(surface.find_entities_filtered{area = {{global.x+10, global.y+11},{global.x+18, global.y+19}}}) do
     entity.destroy()
   end
@@ -53,6 +112,26 @@ local refresh_silo = function()
   silo.destructible = false
   silo.minable = false
 
+  -- Restore silo content and status (we re-create from scratch to avoid cheese and inconsistent states)
+  for n,c in pairs(module_inventory) do
+    silo.get_module_inventory().insert({name=n,count=c})
+  end
+
+  for n,c in pairs(output_inventory) do
+    silo.get_output_inventory().insert({name=n,count=c})
+  end
+
+  for n,c in pairs(input_inventory) do
+    silo.get_inventory(defines.inventory.assembling_machine_input).insert({name=n,count=c})
+  end
+
+  for n,c in pairs(rocket_inventory) do
+    silo.get_inventory(defines.inventory.rocket_silo_rocket).insert({name=n,count=c})
+  end
+
+  silo.rocket_parts = rocket_parts
+
+  -- Put some concrete around it to be pretty (plus it leaves old concrete)
   local tiles = {}
   local i = 1
   for dx = -7,7 do
@@ -196,6 +275,56 @@ local register_commands = function()
       local player = game.players[e.player_index]
       player.print("Moved silo by " .. tostring(offset))
     end
+
+  -- Control costs/internal values
+  commands.add_command("addrocket", "Add (or remove) rockets to the win condition", function(e)
+    rw = tonumber(e.parameter)
+    if rw ~= nil then
+      if not global.rockets_to_win then
+        global.rockets_to_win = rw
+      else
+        global.rockets_to_win = global.rockets_to_win + rw
+      end
+    end
+    if global.rockets_to_win < 1 then global.rockets_to_win = 1 end
+
+    game.print("Rocket launches to win: " .. tostring(global.rockets_to_win).." with "..tostring(global.rockets_launched).." launches so far.")
+  end)
+
+  commands.add_command("moveratio", "Set the ratio of move requested to actual move", function(e)
+    cr = tonumber(e.parameter)
+    if cr ~= nil then
+      global.move_cost_ratio = cr
+      if global.move_cost_ratio < 1 then global.move_cost_ratio = 1 end
+    end
+    game.print("You need to request a "..tostring(global.move_cost_ratio).." tile move to actually move the silo 1 tile.")
+  end)
+
+  commands.add_command("rocketcost", "Set the cost in tiles for an additional launch", function(e)
+    rs = tonumber(e.parameter)
+    if rs ~= nil then
+      global.rocket_step = rs
+      if global.rocket_step < 1 then global.rocket_step = 1 end
+    end
+    game.print("Extra launches now cost "..tostring(global.rocket_step).." tiles each.")
+  end)
+
+  commands.add_command("maxdistance", "Set the maximum distance for the silo, adding tiles past this will add launches", function(e)
+    md = tonumber(e.parameter)
+    if md ~= nil then
+      global.max_distance = md
+      if global.max_distance < 2*deathworld_boundary then global.max_distance = 2*deathworld_boundary end
+    end
+    game.print("The silo can now be at most "..tostring(global.max_distance).." tiles away.")
+  end)
+
+  commands.add_command("movestep", "Set the number of actual tiles the silo moves in each step", function(e)
+    ms = tonumber(e.parameter)
+    if ms ~= nil then
+      global.move_step = ms
+      if global.move_step < 1 then global.move_step = 1 end
+    end
+    game.print("The silo will move in "..tostring(global.move_step).." tile increments.")
   end)
 end
 
@@ -205,11 +334,27 @@ frontier.events =
   [defines.events.on_chunk_generated] = on_chunk_generated,
   [defines.events.on_research_finished] = on_research_finished,
   [defines.events.on_player_changed_position] = on_player_changed_position,
+  [defines.events.on_rocket_launched] = on_rocket_launched,
 }
 
 frontier.on_init = function()
   global.version = version
   global.silo_created = false
+
+  -- Rockets/silo location management
+  global.rockets_to_win = 1
+  global.rockets_launched = 0
+  
+  global.move_cost_ratio = 1 -- If the multipler is 2, you need to buy 2x the tiles to actually move 1x
+  global.move_step = 500 -- By default, we only move 500 tiles at a time
+  
+  global.rocket_step = 500 -- How many "tiles" past the max distance adds a launch
+
+  global.max_distance = 100000 -- By default, 100k tiles max to the right
+  
+  global.move_buffer = 0 -- How many tiles we haven't currently reflected (between +move_step and -move_step)
+  global.contributors = {} -- List of contributors so far (so that we can print when we actually move the silo)
+
   register_commands()
 end
 
